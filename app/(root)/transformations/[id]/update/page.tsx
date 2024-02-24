df935dc70f61c8710087a52adb895a5facaf5b93
Ehteshamali-889 <webdeveloper3628@gmail.com>
2024-02-24 16:30:32 +0500

    basic setup

---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UpdateTransaformationPage = () => {
+  return (
+    <div>UpdateTransaformationPage</div>
+  )
+}
+
+export default UpdateTransaformationPage
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddTransaformationTypePage = () => {
+  return (
+    <div>AddTransaformationTypePage</div>
+  )
+}
+
+export default AddTransaformationTypePage
